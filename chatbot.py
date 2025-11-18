46b3fdcce8a57f8fa68a6315ec9929bf1c49d323
Divya Sain <saindivya77@gmail.com>
2025-11-19 01:22:30 +0530

    first chatbot done

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,16 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
+import os
+model=ChatOpenAI(
+    model="gryphe/mythomax-l2-13b",
+    openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+    openai_api_base="https://openrouter.ai/api/v1"
+)
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    result = model.invoke(user_input)
+    print("Ai: ",result.content)
