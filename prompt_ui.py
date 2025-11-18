71d7fa02312e2597a3ecc362036f5eecb1d0ba1d
Divya Sain <saindivya77@gmail.com>
2025-11-18 23:22:54 +0530

    final

--- prompt_ui.py
+++ prompt_ui.py
@@ -2,7 +2,8 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate ,load_prompt
+
 
 load_dotenv()
 st.header("Research Tool")
@@ -13,45 +14,24 @@ model = ChatOpenAI(
     openai_api_base="https://openrouter.ai/api/v1",
 )
 
-paper_input = st.selectbox("Select Research Paper Name", [
-    "Attention Is All You Need",
-    "BERT: Pre-training of Deep Bidirectional Transformers",
-    "GPT-3: Language Models are Few-Shot Learners",
-    "Diffusion Models Beat GANs on Image Synthesis"
-])
 
-style_input = st.selectbox("Select Explanation Style", [
-    "Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"
-])
+st.header('Reasearch Tool')
 
-length_input = st.selectbox("Select Explanation Length", [
-    "Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"
-])
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-template = PromptTemplate(
-    input_variables=["paper_input", "style_input", "length_input"],
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
-1. Mathematical Details:
-   - Include relevant mathematical equations if present.
-   - Use intuitive code snippets where applicable.
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-2. Analogies:
-   - Use relatable analogies to simplify complex ideas.
+template = load_prompt('template.json')
 
-If information is missing, respond: "Insufficient information available".
-"""
-)
 
-if st.button("Summarize"):
+
+if st.button('Summarize'):
     chain = template | model
     result = chain.invoke({
-        "paper_input": paper_input,
-        "style_input": style_input,
-        "length_input": length_input
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
     })
-    st.write(result.content)
-    
+st.write(result.content)
