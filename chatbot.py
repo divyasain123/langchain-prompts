ef6679a2472c098b0288ae8fd0c0a26054ec1c3e
Divya Sain <saindivya77@gmail.com>
2025-11-19 16:58:46 +0530

    add

--- chatbot.py
+++ chatbot.py
@@ -7,10 +7,22 @@ model=ChatOpenAI(
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     openai_api_base="https://openrouter.ai/api/v1"
 )
-
-while True:
+#chat history not getting saved in this example
+"""while True:
     user_input = input("You: ")
     if user_input.lower() in ["exit", "quit"]:
         break
     result = model.invoke(user_input)
     print("Ai: ",result.content)
+"""
+
+chat_history=[]
+while True:
+    user_input = input("You: ")
+    chat_history.append(user_input)
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
+    print("Ai: ",result.content)
+    print("Chat History: ",chat_history)
